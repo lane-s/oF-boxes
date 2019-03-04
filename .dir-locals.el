0d4148ae7eebc98f36cfbfd1cb4f3acb304ce146
Lane Spangler <las4vc@virginia.edu>
2019-03-04 16:56:06 -0600

    Add dir-locals to include of headers in clang's include path

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((eval . (setq flycheck-clang-include-path
+                       (list (expand-file-name "/Users/lspangler/Documents/Lane/Dev/GameDev/openFrameworks/libs/openFrameworks/")))))))
